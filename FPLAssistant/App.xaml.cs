5d8f2a2a5d8879b2bfe70808236fff1075ea1924
DeanFord7 <DeanoFord7@gmail.com>
2025-01-26 18:03:45 +0000

    Add project files.

---
+++ FPLAssistant/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FPLAssistant
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
